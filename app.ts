cfbaf9c218c78a042b39a69b96959883ac5948d1
Yulia Kovalenko <kovalenkoyuliadev@gmail.com>
2023-06-20 09:32:27 +0200

    add test1

--- app.ts
+++ app.ts
@@ -1,4 +1,14 @@
-function getFullName(firstName: string, suranme: string): string {
-    return `${firstName} ${suranme}`;
+function getFullName(userEntity: {firstName: string, surname: string}): string {
+    return `${userEntity.firstName} ${userEntity.surname}`;
 }
 
+const user = {
+    firstName: 'Alex',
+    surname: 'Alvarez',
+    city: 'Madrid',
+    age: 30,
+    skills: {
+        dev: true,
+        devops: true
+    }
+}
